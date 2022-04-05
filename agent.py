1d1dfe17faa3b4076acde7b3444251ec3557319d
SiddharthSingi <siddharth.singi@gmail.com>
2022-04-05 15:36:19 -0400

    alg2 thresholding, smoothing corrected

--- agent.py
+++ agent.py
@@ -1,12 +1,14 @@
+from threading import currentThread
 from environment import Grid, ExpertGrid
 import numpy as np
 from matplotlib import pyplot as plt
 import os
 import math
+import seaborn as sns
 # Uncomment the algorithm you'd like to run at the end
 
 class VanillaAgent():
-    def __init__(self, table_size, grid, fname) -> None:
+    def __init__(self, table_size, grid, dpath) -> None:
         
         self.qtable = np.zeros(shape=table_size)    # Q-Learning table
         self.rollout_tbl = np.zeros(shape=np.append(np.array(table_size), 3))
@@ -15,9 +17,11 @@ class VanillaAgent():
         self.gamma = 0.9
         self.rewards_list = []
         self.smoothing_num = 1000
-        self.fname = fname
+        self.fname = dpath
         self.lr = 0.1   # Q value learning rate
         self.epsilon = 0.15  # Exploration epsilon
+        self.exploration_count = np.zeros((10, 10, 4))
+
         # Debugging
         self.avoid_states = [[1,2], [1,3], [2,4], [3,4], [4,2], [4,3], [2,1], [3,1], [4,4], [4,5],\
             [4,6], [5,7], [6,7], [7,6], [7,5], [7,4], [6,3], [5,3]]
@@ -62,6 +66,7 @@ class VanillaAgent():
             self.epsilon = eps_start*math.exp(-lmda*ep)
             while not done:
                 action = self.choose_action(prev_obs)
+                self.exploration_count[prev_obs[0], prev_obs[1], action] += 1
                 # print(self.grid.done)
                 # print('prev obs: ', prev_obs)
                 obs, reward, done = self.grid.step(action)
@@ -72,13 +77,15 @@ class VanillaAgent():
 
             self.rewards_list.append(tot_reward)
         
-        # Save the qtable
+        # Save the qtable and exploration count
         np.save(os.path.join(self.fname, 'qtable.npy'), self.qtable)
+        np.save(os.path.join(self.fname, 'exploration'), self.exploration_count)
 
         # Plot rewards
         x = np.arange(0, len(self.rewards_list), self.smoothing_num)
         x = np.append(x, len(self.rewards_list)-1)
-        y = [np.average(self.rewards_list[i:i+1]) for i in range(len(x))]
+        y = [np.average(self.rewards_list[x[i]:x[i+1]]) for i in range(len(x)-1)]
+        x = x[:-1]
         fig, ax0 = plt.subplots(figsize=(8,6))
         ax0.set_title('Rewards per episode')
         ax0.plot(x, y)
@@ -137,7 +144,9 @@ class VanillaAgent():
         # Plot rewards
         x = np.arange(0, len(self.rewards_list), smoothing)
         x = np.append(x, len(self.rewards_list)-1)
-        y = [np.average(self.rewards_list[i:i+1]) for i in range(len(x))]
+        y = [np.average(self.rewards_list[x[i]:x[i+1]]) for i in range(len(x)-1)]
+        x = x[:-1]
+        
         fig, ax0 = plt.subplots(figsize=(8,6))
         ax0.set_title('Rewards per episode')
         ax0.plot(x, y)
@@ -220,6 +229,8 @@ class ALG1(VanillaAgent):
     def __init__(self, table_size, grid, fname) -> None:
         super().__init__(table_size, grid, fname)
         self.action_space = 5
+        self.exploration_count = np.zeros((10, 10, 5))
+
 
 
 class ALG2(VanillaAgent):
@@ -228,6 +239,7 @@ class ALG2(VanillaAgent):
         self.q_lr = 0.1
         self.m_lr = 0.1
         self.v_lr = 0.1
+        self.exploration_count = np.zeros((10, 10, 4))
 
     def learn(self, prev_state, action, reward, new_state):
 
@@ -273,7 +285,6 @@ class ALG2(VanillaAgent):
             # We will still choose the action based on the highest q-value amonsgst all actions
             action = np.argmax(self.qtable[state[0], state[1], :, 0])
         return action
-
     
     def play(self, num_episodes, decay_ep=False, eps_start=0.15, eps_end=0.01, retrain=False):
         
@@ -287,10 +298,11 @@ class ALG2(VanillaAgent):
             tot_reward = 0
             while not done:
                 action = self.choose_action(prev_obs)
+                self.exploration_count[prev_obs[0], prev_obs[1], action] += 1
                 # print('prev obs: ', prev_obs)
                 obs, reward, done = self.grid.step(action)
-                if ep==8000:
-                    print(f'prev_state, action, next_state, reward: {prev_obs}, {action}, {obs}, {reward}, {done}')
+                # if ep==8000:
+                #     print(f'prev_state, action, next_state, reward: {prev_obs}, {action}, {obs}, {reward}, {done}')
                 # if obs[0]==3 and obs[1]==3:
                 #     print(f'Reached trap, reward, tot_r, done: {reward}, {tot_reward}, {done}')
                 
@@ -301,14 +313,19 @@ class ALG2(VanillaAgent):
             self.rewards_list.append(tot_reward)
         
 
-        # Save the qtable
+        # Save the qtable, exploration_count table
         np.save(os.path.join(self.fname, 'qtable.npy'), self.qtable)
+        np.save(os.path.join(self.fname, 'exploration.npy'), self.exploration_count)
 
         # Visualizations
         # Plotting Rewards
         x = np.arange(0, len(self.rewards_list), self.smoothing_num)
         x = np.append(x, len(self.rewards_list)-1)
-        y = [np.average(self.rewards_list[i:i+1]) for i in range(len(x))]
+        y = [np.average(self.rewards_list[x[i]:x[i+1]]) for i in range(len(x)-1)]
+        x = x[:-1]
+
+        print('X: ', x, '\n')
+        print('Y: ', y)
         fig, ax0 = plt.subplots(figsize=(8,6))
         ax0.set_title('Rewards per episode')
         ax0.plot(x, y)
@@ -368,6 +385,58 @@ class ALG2(VanillaAgent):
             bbox_inches ="tight",\
             dpi=250)
 
+    def get_action_table(self, qtable, threshold):
+        """
+        returns an action table where action=4 if variance is above the threshold
+        qtable: shape=(10,10,4,3) Learned from ALG2
+        threshold: threshold for the variance value
+        """
+        rows, cols = np.indices((10,10))
+        rows = rows.reshape(-1)
+        cols = cols.reshape(-1)
+        best_actions = np.argmax(qtable[:, :, :, 0], axis=2).reshape(-1)
+        variances = qtable[:, :, :, 2][rows, cols, best_actions]
+        # best_actions = best_actions.reshape(10,10)
+        action_table = np.where(variances<threshold, best_actions, 4).reshape(10,10)
+
+        # fig, ax = plt.subplots()
+        # ax = sns.heatmap(action_table, annot=True, fmt=".1f", cmap='cool', linewidths=.5)
+        # plt.show()
+
+        return action_table
+    
+    def threshold_rollout(self, num_episodes, max_thresh, thresh_spacing, qtable_f):
+        grid = ExpertGrid()
+        qtable = np.load(qtable_f)
+        thresh_rewards = []
+        thresholds = []
+        for thresh in range(0, max_thresh, thresh_spacing):
+            action_table = self.get_action_table(qtable, thresh)
+            cur_thresh_rewards = []
+            for ep in range(num_episodes):
+                done = False
+                prev_obs = grid.reset()
+                tot_reward = 0
+                while not done:
+                    action = action_table[prev_obs[0], prev_obs[1]]
+                    obs, reward, done = grid.step(action)
+                    tot_reward+=reward
+                    prev_obs = obs
+                cur_thresh_rewards.append(tot_reward)
+
+            thresh_avg_reward = sum(cur_thresh_rewards) / len(cur_thresh_rewards)
+            thresh_rewards.append(thresh_avg_reward)
+            thresholds.append(thresh)
+
+        thresh_rewards = np.array(thresh_rewards)
+        thresholds = np.array(thresholds)
+        np.savez(os.path.join(self.fname, 'thresholdrollout.npz'), thresh_rewards, thresholds)
+
+        fig, ax = plt.subplots()
+        ax = sns.barplot(x=thresholds, y=thresh_rewards)
+        plt.show()
+
+        return
 
 class MonteCarlo(VanillaAgent):
     def __init__(self, table_size, grid) -> None:
@@ -420,8 +489,10 @@ class MonteCarlo(VanillaAgent):
         
         # Plot rewards
         x = np.arange(0, len(self.rewards_list), self.smoothing_num)
-        y = [np.average(self.rewards_list[i:i+self.smoothing_num]) for i in x[:-1]]
-        y = np.array(self.rewards_list)[x]
+        x = np.append(x, len(self.rewards_list)-1)
+        y = [np.average(self.rewards_list[x[i]:x[i+1]]) for i in range(len(x)-1)]
+        x = x[:-1]
+        
         fig, ax = plt.subplots(figsize=(8,15))
         ax.plot(x, y)
         plt.show()
@@ -474,33 +545,82 @@ class MonteCarlo(VanillaAgent):
 
 # Vanilla
 # grid = Grid()
-# dpath = 'Outputs/Vanilla/Expertrun2'
-# os.makedirs(dpath)
+# dpath = 'Outputs/Vanilla/10M.7'
+# num_episodes = 10000000
+# ep_decay = 5e6
+# smoothing_num = 1000
+# eps_start = 0.3
+# eps_end = 0.01
+
+# if not os.path.exists(dpath):
+#     os.makedirs(dpath)
+# else:
+#     raise Exception('This dpath already exists, be more careful dummy!')
 # agent = VanillaAgent([10,10,4], grid, dpath)
-# # agent.play(10000000, 5e6, 0.20, 0.01)
-# agent.policy_rollout(100000, 1000, 'Outputs/Vanilla/expertaction.npy')
+# agent.play(num_episodes, ep_decay, eps_start, eps_end)
+# agent.policy_rollout(100000, smoothing_num)
 # print(repr(np.argmax(agent.qtable, axis=2)))
+
+# f = open(os.path.join(dpath, 'description.txt'), 'w')
+# f.write(f'Vanilla Training\n \
+# episodes: {num_episodes}\n \
+# smoothing num: {smoothing_num}\n \
+# eps_start: {eps_start} \
+# eps_end: {eps_end} \
+# starting_states: states 1 step away from trap.')
+# f.close()
+
+
 
 
 ## ALG1
-grid = ExpertGrid()
-dpath = 'Outputs/ALG1/100k.2'
-os.makedirs(dpath)
-# Our agent has 5 possible actions hence the q table size is 10,10,5
-agent = ALG1([10,10,5], grid, dpath)
-agent.play(100000, False, 0.25, 0.01)
-agent.policy_rollout(100000, 1000)
-print(repr(np.argmax(agent.qtable, axis=2)))
+# grid = ExpertGrid()
+# dpath = 'Outputs/ALG1/100k.6'
+# if not os.path.exists(dpath):
+#     os.makedirs(dpath)
+# else:
+#     raise Exception('This dpath already exists, be more careful dummy!')
+# # Our agent has 5 possible actions hence the q table size is 10,10,5
+# agent = ALG1([10,10,5], grid, dpath)
+# agent.play(800000, 400000, 0.6, 0.07)
+# agent.policy_rollout(100000, 1000)
+# print(repr(np.argmax(agent.qtable, axis=2)))
 
 
-# # ALG2
+
+
+# # # ALG2
+# num_episodes = 10000000
+# ep_decay = 5e6
+# smoothing_num = 1000
+# eps_start = 0.4
+# eps_end = 0.01
+
 # grid = Grid()
-# dpath = 'Outputs/ALG2/10M.2/test' # Output folder name
-# os.makedirs(dpath)
+# dpath = 'Outputs/ALG2/10M.5' # Output folder name
+# if not os.path.exists(dpath):
+#     os.makedirs(dpath)
+# else:
+#     raise Exception('This dpath already exists, be more careful dummy!')
 # # The agent can take 4 actions and for each action it finds the q-value, 2nd reward moment
 # # and the variance of the returs using Bellmann Equations
 # agent = ALG2([10,10,4,3], grid, dpath)
-# agent.play(1000000, eps_start=0.25, eps_end=0.01, retrain=False)
+# agent.play(num_episodes, ep_decay, eps_start, eps_end, retrain=False)
+
+# f = open(os.path.join(dpath, 'description.txt'), 'w')
+# f.write(f'ALG2 Training\n \
+# episodes: {num_episodes}\n \
+# smoothing num: {smoothing_num}\n \
+# ep_decay: {ep_decay}\n \
+# eps_start: {eps_start}\n \
+# eps_end: {eps_end}\n\
+# starting_states: states 1 step away from trap, plus few top left states.')
+# f.close()
+
+# Create the rewards plot with thresholding
+grid = ExpertGrid()
+agent = ALG2([10,10,4,3], grid, 'Outputs/ALG2/10M.4')
+agent.threshold_rollout(200, 8000, 100, 'Outputs/ALG2/10M.4/qtable.npy')
 
 
 # grid = Grid()
